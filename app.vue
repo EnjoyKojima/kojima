1fbe424444308731d18e008163621747c227996c
EnjoyKojima <majiko2627kojima@gmail.com>
2025-01-14 23:10:28 +0900

    たし引き

--- app.vue
+++ app.vue
@@ -1,6 +1,23 @@
+<script setup lang="ts">
+const count = ref(0)
+
+const increment = () => {
+  count.value++
+}
+
+const decrement = () => {
+  count.value--
+}
+</script>
+
 <template>
   <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
+    <div>
+      カウンター: {{ count }}
+    </div>
+    <div>
+      <button @click="increment">増やす</button>
+      <button @click="decrement">減らす</button>
+    </div>
   </div>
 </template>
